0e13af2b3294b0e72f2096b3f49b79b8acfed723
aleksandra94 <oleczkapiotrowska@gmail.com>
2020-05-02 13:09:04 +0200

    Home Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './scss/main.scss'
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
+
 
 function App() {
   return (
---
+++ src/components/HomeHeader.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import decorationURL from '../assets/Decoration.svg'
+import bannerURL from '../assets/Hero Image.png'
+
+const HomeHeader = () => {
+    return (
+        <header className="page__header">
+            <div className="page__header__picture">
+            <img src={bannerURL} />
+            </div>
+            <div className="page__header__container">
+            <ul className="page__header__login">
+                <li><Link to="/login">Zaloguj</Link></li>
+                <li><Link to="/register">Załóż konto</Link></li>
+            </ul>
+            <nav className="page__header__menu">
+                <ul className="page__menu__list">
+                    <li><Link to="/">Start</Link></li>
+                    <li><Link to="/info">O co chodzi?</Link></li>
+                    <li><Link to="/about">O nas</Link></li>
+                    <li><Link to="/charity">Fundacja i organizacje</Link></li>
+                    <li><Link to="/contact">Kontakt</Link></li>
+                </ul>
+            </nav>
+            <section className="page__header__info">
+                <div className="page__header__info__container">
+                <h1>Zacznij pomagać!</h1>
+                <br/>
+                <h1>Oddaj niechciane rzeczy w zufane ręce</h1>
+                <img src={decorationURL} />
+                </div>
+                <section className="page__header__buttons">
+                    <button href="#">Oddaj rzeczy</button>
+                    <button href="#">Zorganizuj zbiórkę</button>
+                </section>
+            </section>
+            </div>
+        </header>
+    )
+}
+
+export default HomeHeader;
